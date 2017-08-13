c54975c2f491b1e2197652dae0320f48fe69ef6e
sena <sena@sena-Inspiron-3442>
2017-08-12 22:18:47 -0300

    Primeiro teste de repositorio

---
+++ teste/src/inicio.java
@@ -0,0 +1,11 @@
+
+public class inicio {
+
+	public static void main(String[] args) {
+		teste tes = new teste();
+		tes.mostra();
+		tes.soma(8);
+		tes.mostra();
+	}
+
+}
---
+++ teste/src/teste.java
@@ -0,0 +1,18 @@
+
+public class teste {
+
+	private int teste = 2;
+	private String teste2 = "ahannn";
+
+	
+public void mostra(){
+	System.out.println("Numero: "+teste+" String: "+teste2);	
+ }
+
+public void soma(int n){
+	teste += n; 
+}
+
+
+
+}
